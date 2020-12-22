44f2b35d4b6043f7e41e60348f947ea721cbb97e
mihaigabriel18 <mihaicalitescu18@gmail.com>
2020-12-23 01:50:51 +0200

    bdd with matrix in every node

--- main.py
+++ main.py
@@ -91,6 +91,52 @@ def fnc_sat(expression_literals):
     fnc_sat_backtracking(0, len(matrix_lit[0]), 0, interpretation, matrix_lit)
 
 
+# def bdd_sat(matrix_lit):
+#     queue = []
+#
+#     root = Node(matrix_lit)
+#     queue.append((root, 0))
+#     while True:
+#         currTuple = queue.pop()
+#         currNode = currTuple[0]
+#         level = currTuple[1]
+#         # reached a leaf node, tree is complete
+#         if level == len(matrix_lit):
+#             queue.insert(0, currTuple)
+#             break
+#
+#         # built left child for negative case
+#         matrix_lit_copy_st = currNode.value.copy()
+#         matrix_lit_copy_st[:, level] *= -1
+#
+#         # built right child for negative case
+#         matrix_lit_copy_dr = currNode.value.copy()
+#         # matrix_lit_copy_dr[:, level] *= 1
+#
+#         # create left and right nodes
+#         node_l = Node(matrix_lit_copy_st)
+#         node_r = Node(matrix_lit_copy_dr)
+#         # add left and right child
+#         currNode.left = node_l
+#         currNode.right = node_r
+#         # adding nodes to queue
+#         queue.append((node_l, level + 1))
+#         queue.append((node_r, level + 1))
+#
+#     queue.clear()
+#     # replace leaf values with '1's and '0's
+#     while len(queue) is not 0:
+#         currTuple = queue.pop()
+#         currNode = currTuple[0]
+#         currMatrix = currNode.value
+#         for i in range(0, len(currMatrix[:, 0])):
+#             if 1 not in currMatrix[i, :]:
+#                 currNode.value = False
+#                 break
+#             # This code is reached if equation evaluates to true
+#             currNode.value = True
+
+
 if __name__ == '__main__':
     expression = input()
     fnc_sat(expression)
